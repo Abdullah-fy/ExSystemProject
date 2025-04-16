7f940d67b3004f8ba7b889d7c7d73e1947ed8976
abdom <abdo@email.com>
2025-04-16 21:41:33 +0200

    auth

--- ExSystemProject/Repository/BranchRepo.cs
+++ ExSystemProject/Repository/BranchRepo.cs
@@ -1,12 +1,23 @@
 ﻿using ExSystemProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExSystemProject.Repository
 {
     public class BranchRepo: GenaricRepo<Branch>
     {
+        private readonly ExSystemTestContext context;
+
         public BranchRepo(ExSystemTestContext context): base(context)
         {
-            
+            this.context = context;
+        }
+        public List<Branch> GetAllActive()
+        {
+            return context.Branches.Where(a => a.Isactive == true).ToList();
+        }
+        public bool Exists(int id)
+        {
+            return context.Branches.Any(e => e.BranchId == id);
         }
     }
 }
---
+++ ExSystemProject/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExSystemProject.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
