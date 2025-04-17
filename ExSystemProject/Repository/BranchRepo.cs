d34ac079d156fc405935080e0d2fc1e746942a1f
tarek mohamed <tarekm@gmail.com>
2025-04-17 21:55:03 +0200

    branch and track crud

--- ExSystemProject/Repository/BranchRepo.cs
+++ ExSystemProject/Repository/BranchRepo.cs
@@ -1,12 +1,21 @@
 ﻿using ExSystemProject.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExSystemProject.Repository
 {
-    public class BranchRepo: GenaricRepo<Branch>
+    public class BranchRepo : GenaricRepo<Branch>
     {
-        public BranchRepo(ExSystemTestContext context): base(context)
+        ExSystemTestContext _context;
+        public BranchRepo(ExSystemTestContext context) : base(context)
         {
-            
+            _context = context;
+        }
+
+        public List<Track> GetTracksByBranchId(int id){
+            return _context.Tracks
+             .FromSqlRaw("EXEC sp_GetTracksByBranchId @BranchId", new SqlParameter("@BranchId", id))
+             .ToList();
         }
     }
 }
--- ExSystemProject/Repository/TrackRepo.cs
+++ ExSystemProject/Repository/TrackRepo.cs
@@ -1,12 +1,37 @@
-﻿using ExSystemProject.Models;
+﻿using ExSystemProject.DTOS;
+using ExSystemProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExSystemProject.Repository
 {
     public class TrackRepo:GenaricRepo<Track>
     {
+        ExSystemTestContext _context;
         public TrackRepo(ExSystemTestContext constext) : base(constext)
         {
+            this._context = constext;
+        }
 
+
+        public List<TrackDTO> GetAllWithBranch()
+        {
+            List<Track> tracks = _context.Tracks.Include(t => t.Branch).ToList();
+            List<TrackDTO> trackDTO = new List<TrackDTO>();
+            foreach (var track in tracks) {
+
+                trackDTO.Add(new TrackDTO()
+                {
+                    TrackId = track.TrackId,
+                    TrackName = track.TrackName,
+                    TrackDuration = track.TrackDuration,
+                    TrackIntake = track.TrackIntake,
+                    IsActive = track.IsActive,
+                    BranchId = track.BranchId,
+                    BranchName = track.Branch.BranchName
+                });
+            }
+           
+            return trackDTO;
         }
     }
 }
